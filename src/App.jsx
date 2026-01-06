1f12fbe0fdae7b156ee4de5d1eca707f50e77c72
Aj-Aralar <ajorick.aralar@gmail.com>
2026-01-06 04:49:14 -0500

    feat(type): Now include the pokemon type

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,36 @@ import { useState } from "react"
 import { getRandomPokemon } from "./utils/getRandomPokemon"
 
 function App() {
-  const [pokemon, setPokemon] = useState({})
+  // Initial pokemon state
+  const [pokemon, setPokemon] = useState(null)
+
+  // Generate random pokemon using API
   const generatePokemon = async () => {
-    const data = await getRandomPokemon()
-    setPokemon(data)
+    try {
+      const randomPokemon = await getRandomPokemon()
+      console.log(randomPokemon)
+      setPokemon(randomPokemon)
+    } catch (error) {
+      console.error(error)
+    }
   }
 
   return (
     <>
       <button onClick={generatePokemon}>Random Pokemon</button>
-      {pokemon && <h1>{pokemon.name}</h1>}
+
+      {/* If pokemon is "truthy, display" */}
+      {pokemon && (
+        <div>
+          {/* Pokemon name */}
+          <h1>{pokemon.name}</h1>
+
+          {/* Pokemon types */}
+          {pokemon.types.map((type, index) => (
+            <h1 key={index}>{type.type.name}</h1>
+          ))}
+        </div>
+      )}
     </>
   )
 }
--- src/utils/getRandomPokemon.js
+++ src/utils/getRandomPokemon.js
@@ -1,8 +1,12 @@
+// Import API URL from env file
 const POKEAPI_URL = import.meta.env.VITE_POKEAPI_URL
 
 export const getRandomPokemon = async () => {
+    // Random number generator (0 to 100)
     const randomNum = Math.floor(Math.random() * 100) + 1
+
     try {
+        // API endpoint
         const response = await fetch(`${POKEAPI_URL}/pokemon/${randomNum}`)
         if (!response.ok) {
             throw new Error(`getRandomPokemon response status: ${response.status}`)
